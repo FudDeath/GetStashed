89c97491daf022703f0aae7f308ffbc166a16413
notdeath <103466637+FudDeath@users.noreply.github.com>
2024-08-21 18:30:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { WalletKitProvider } from '@mysten/wallet-kit';
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GetstashedFrontend from './ZksendFrontend.js'; // Adjust the import path as necessary
+
+const queryClient = new QueryClient();
+const networks = {
+  mainnet: { url: getFullnodeUrl('mainnet') },
+  testnet: { url: getFullnodeUrl('testnet') },
+  devnet: { url: getFullnodeUrl('devnet') },
+};
 
 function App() {
   return (
-    <WalletKitProvider>
-      <GetstashedFrontend />
-    </WalletKitProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="mainnet">
+        <WalletProvider>
+          <GetstashedFrontend />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   );
 }
 
